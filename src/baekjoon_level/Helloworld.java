2ddcc9da75f0888254dbca4230654f70b503e1ea
asdvsvs <gag973000@gmail.com>
2022-11-20 02:04:34 +0900

    11.20

---
+++ src/baekjoon_level/Helloworld.java
@@ -0,0 +1,77 @@
+//https://www.acmicpc.net/problem/2557
+
+package baekjoon_level;
+
+
+
+
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//	}
+//}
+//import java.util.Scanner;
+//public class Main{
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int a,b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
+//	}
+//}
+//import java.util.Scanner;
+//public class Main{
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String s= sc.nextLine();
+//		System.out.println(s+"??!");
+//	}
+//}
+
+//import java.util.Scanner;
+//public class Main{
+//	public static void main(String[] args) {
+//		Scanner sc= new Scanner(System.in);
+//		int[] n = new int[6];
+//		for (int i = 0; i < n.length; i++) {
+//			n[i]=sc.nextInt();
+//		}
+//		int[] p= new int[6];
+//		for (int i = 0; i < n.length; i++) {
+//			if(0<=i&&i<2)	p[i]=1-n[i];
+//			else if(1<i&&i<5) p[i]=2-n[i];
+//			else p[i]=8-n[i];
+//		}
+//		for (int i = 0; i < n.length; i++) {
+//			System.out.print(p[i]+" ");
+//		}
+//	}
+//}
+
+//백준 15596번, 정수 N개의 합
+//import java.util.Scanner;
+//public class Main{
+//	public int main(){
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int sum=0;
+//		int[] a= new int[n];
+//		for (int i = 0; i < a.length; i++) {
+//			a[i] =sc.nextInt();
+//			sum= sum+ a[i];
+//		}
+//		return sum;
+//	}
+//}
+
+
+
+
+
+
+
