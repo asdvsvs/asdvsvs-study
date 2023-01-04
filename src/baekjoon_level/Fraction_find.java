73e24f857f9532dd7025e63ab3cde91827b15417
asdvsvs <gag973000@gmail.com>
2023-01-04 20:41:31 +0900

    23.1.4

--- src/baekjoon_level/Fraction_find.java
+++ src/baekjoon_level/Fraction_find.java
@@ -7,47 +7,34 @@ import java.io.InputStreamReader;
 public class Fraction_find {
 	public void fraction_find() throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int x = Integer.parseInt(bf.readLine());
-		int n[][]=new int[x][x];
+		int n = Integer.parseInt(bf.readLine());
 		int cnt=1;
+		int t=4;
+		int k=2;
+		int x =1;
+//		y/x
 		
-		int a=0,b=0;
-		
-		loopOut:
-		while(cnt<=x){
+		while(cnt<n ){
+			if(x%2==1){
+				cnt=cnt+1;
+				x++;
+			}
+			else if(x%2==0){
+				if(k>n){
+					
+				}
+				cnt=cnt+t;
+				t=t+4;
+				x++;
+				
+			}
 			
-			int ta=a,tb=b;
-			if(a==0&&b==0){
-				n[a][b]=cnt++;
-				b++;
-			}
-			if(a<b){
-				while(a<=tb){
-					if(cnt==x){
-						System.out.println((a+1)+"/"+(b+1));
-						break loopOut;
-					}
-					n[a][b]=cnt++;
-					if(a==tb)break;
-					a++;
-					b--;
-				}
-			}
-			else if(a>b){
-				while(b<=ta){
-					if(cnt==x){
-						System.out.println((a+1)+"/"+(b+1));
-						break loopOut;
-					}
-					n[a][b]=cnt++;
-					if(b==ta)break;
-					a--;
-					b++;
-				}
-			}
 		}
 		
+		System.out.println(cnt+" x:"+x);
 		
 		
+//		12 67 1516 2829 4546 6566 8990
+//		1 34 1011 2122 3637
 	}
 }
