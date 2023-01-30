1315f86e4d72a19db5b102cef4307181480bf5ea
asdvsvs <gag973000@gmail.com>
2023-01-30 21:14:14 +0900

    23.1.30

--- src/baekjoon_level/Baekjoon_18870.java
+++ src/baekjoon_level/Baekjoon_18870.java
@@ -1,30 +1,31 @@
 package baekjoon_level;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Baekjoon_18870 {
 	public void baekjoon_18870 () throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
+		ArrayList<Integer> arr = new ArrayList<>();
 		
 		int N = Integer.parseInt(bf.readLine());
 		int[] num = new int [N];
-		int[] num_sort = new int [N];
+		int[] result = new int[N];
 		st = new StringTokenizer(bf.readLine());
 		for (int i = 0; i < N; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
+			arr.add(num[i]);
+		}
+		List<Integer> newarr=arr.stream().distinct().collect(Collectors.toList());
+		Collections.sort(newarr);
+		for (int i = 0; i < N; i++) {
+			result[i]=newarr.indexOf(num[i]);
 		}
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(num[i]>num[j]){
-					
-					num_sort[i]++;
-				}
-			}
-			bw.write(String.valueOf(num_sort[i])+" ");
+			bw.write(result[i] +" ");
 		}
-		
 		
 		bw.flush();
 		bw.close();
